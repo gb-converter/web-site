ecad29a48bb1df6bf3b5f094f4c0084be3a0ca71
Viktor Filinskij <v.filinskij@gmail.com>
2020-11-11 03:43:44 +0200

    Set place to store static and media files during python manage.py collectstatic

--- website/website/settings.py
+++ website/website/settings.py
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = '/var/www/website/media/'
+STATIC_ROOT = '/var/www/website/static/'
