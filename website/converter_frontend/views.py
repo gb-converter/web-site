da7c74c1b389ba0eca5f9be9c0c70ac7d12204c4
Viktor Filinskij <v.filinskij@gmail.com>
2020-11-20 19:27:38 +0200

    Remove unnecessary views Relates #16, Change converter view to provide data in converter form.

--- website/converter_frontend/views.py
+++ website/converter_frontend/views.py
@@ -1,53 +1,91 @@
+import os
+import json
+
 from django.shortcuts import render
 from django.template import Template, Context
 from django.template.loader import get_template, render_to_string
 from django.http import HttpRequest
 from django.http import HttpResponse
+from django.utils.html import mark_safe
 
 
 # Create your views here.
 # context variable value take its place in html under soname placeholder
-def about(request):
-    return HttpResponse(
-        render_to_string(
-            'about.html',
-            {'description': 'Сдесь обычно стоит: Про этот сайт'}
-        )
-    )
+# def about(request):
+#     return HttpResponse(
+#         render_to_string(
+#             'about.html',
+#             {'description': 'Сдесь обычно стоит: Про этот сайт'}
+#         )
+#     )
 
 
 def contacts(request):
     # just display word about by opening /about/ uri
     # return HttpResponse('Контактная информация')
     template = get_template('contacts.html')
-    context = {'description': 'Сдесь обычно стоит: Главная Страница'}
+    context = {
+        'project_home': 'https://github.com/gb-converter?tab=projects',
+        'project_components': {
+            'api': 'https://github.com/gb-converter/parser',
+            'parser': 'https://github.com/gb-converter/parser',
+            'web': 'https://github.com/gb-converter/web-site'
+        }
+
+    }
+
     return HttpResponse(
         template.render(context)
     )
 
 
 def converter(request):
-    return render(request, 'converter.html')
 
+    path = os.path.join((os.getcwd()), 'data_file.json')
 
-def index(request):
-    CONST = True
+    with open(path, 'r',  encoding='utf-8') as data:
+        currencies_data = json.load(data)
 
-    template = Template('<h1>{{ variable }}</h1>')
+    # Extract date from dictionary
+    currencies_date = currencies_data.pop("Date")
 
-    if CONST:
-        context_value = {'description': 'Главная страница'}
-    else:
-        context_value = {'variable': 'Совсем не главная страница'}
+    # Get currency info, a dict in form:
+    #   "AUD": {
+    #     "Цифр. код": "036",
+    #     "Символ": "$",
+    #     "Единица": "1",
+    #     "Валюта": "Австралийский доллар",
+    #     "Курс": "55,5784"
+    #   }
 
-    context = Context(context_value)
-
-    return HttpResponse(template.render(context))
-
-
-def home(request):
-    template = get_template('index.html')
-    context = {'description': 'Сдесь обычно стоит: Домашняя страница'}
-    return HttpResponse(
-        template.render(context)
+    return render(
+        request,
+        'converter.html',
+        {
+            'currency_info': currencies_data,
+            'currencies_rate_date': currencies_date
+        }
     )
+
+
+# def index(request):
+#     CONST = True
+#
+#     template = Template('<h1>{{ variable }}</h1>')
+#
+#     if CONST:
+#         context_value = {'description': 'Главная страница'}
+#     else:
+#         context_value = {'variable': 'Совсем не главная страница'}
+#
+#     context = Context(context_value)
+#
+#     return HttpResponse(template.render(context))
+
+
+# def home(request):
+#     template = get_template('index.html')
+#     context = {'description': 'Сдесь обычно стоит: Домашняя страница'}
+#     return HttpResponse(
+#         template.render(context)
+#     )
