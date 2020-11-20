b4c32249b83693b3144b9ed73c2bdcd020676671
Viktor Filinskij <v.filinskij@gmail.com>
2020-11-20 21:50:28 +0200

    cleanup views from unnecessary code

--- website/converter_frontend/views.py
+++ website/converter_frontend/views.py
@@ -2,39 +2,18 @@ import os
 import json
 
 from django.shortcuts import render
-from django.template import Template, Context
-from django.template.loader import get_template, render_to_string
-from django.http import HttpRequest
-from django.http import HttpResponse
-from django.utils.html import mark_safe
-
-
-# Create your views here.
-# context variable value take its place in html under soname placeholder
-# def about(request):
-#     return HttpResponse(
-#         render_to_string(
-#             'about.html',
-#             {'description': 'Сдесь обычно стоит: Про этот сайт'}
-#         )
-#     )
 
 
 def contacts(request):
-    # just display word about by opening /about/ uri
-    # return HttpResponse('Контактная информация')
-    template = get_template('contacts.html')
     return render(
         request,
         'contacts.html',
         {
-            'context': {
-                'project_home': 'https://github.com/gb-converter?tab=projects',
-                'project_components': {
-                    'api': 'https://github.com/gb-converter/parser',
-                    'parser': 'https://github.com/gb-converter/parser',
-                    'web': 'https://github.com/gb-converter/web-site'
-                }
+            'project_components': {
+                'Домашняя страница проекта': 'https://github.com/gb-converter?tab=projects',
+                'Репозиторий компонента апи': 'https://github.com/gb-converter/api',
+                'Репозиторий компонента парсер': 'https://github.com/gb-converter/parser',
+                'Репозиторий компонента веб сайт': 'https://github.com/gb-converter/web-site'
             }
         }
     )
@@ -44,8 +23,11 @@ def converter(request):
 
     path = os.path.join((os.getcwd()), 'data_file.json')
 
-    with open(path, 'r',  encoding='utf-8') as data:
-        currencies_data = json.load(data)
+    try:
+        with open(path, 'r',  encoding='utf-8') as data:
+            currencies_data = json.load(data)
+    except FileNotFoundError:
+        os.system(f'python parser.py --path {os.getcwd()}')
 
     # Extract date from dictionary
     currencies_date = currencies_data.pop("Date")
@@ -68,25 +50,3 @@ def converter(request):
         }
     )
 
-
-# def index(request):
-#     CONST = True
-#
-#     template = Template('<h1>{{ variable }}</h1>')
-#
-#     if CONST:
-#         context_value = {'description': 'Главная страница'}
-#     else:
-#         context_value = {'variable': 'Совсем не главная страница'}
-#
-#     context = Context(context_value)
-#
-#     return HttpResponse(template.render(context))
-
-
-# def home(request):
-#     template = get_template('index.html')
-#     context = {'description': 'Сдесь обычно стоит: Домашняя страница'}
-#     return HttpResponse(
-#         template.render(context)
-#     )
