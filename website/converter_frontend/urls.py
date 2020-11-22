d8c53d160078470cfbc15be321583af2d40847e4
Viktor Filinskij <v.filinskij@gmail.com>
2020-11-22 14:44:51 +0200

    Relates #20 Change links used in base.html template from static to ones generated by django template language. Add names for each url dispatcher in converter_frontend application, url namespace

--- website/converter_frontend/urls.py
+++ website/converter_frontend/urls.py
@@ -7,6 +7,6 @@ from .views import (
 app_name = 'converter_frontend'
 
 urlpatterns = [
-    path('', converter),
-    path('contacts/', contacts),
+    path('', converter, name='converter'),
+    path('contacts/', contacts, name='contacts'),
 ]
