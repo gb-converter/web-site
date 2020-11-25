9b8e8b4a1b676703067c988a4361efbde719dd5e
Viktor Filinskij <v.filinskij@gmail.com>
2020-11-25 23:12:48 +0200

    Relates #25 Create 2 tables, first table contain 5 fields currency id, currency international iso 3 letter code, numerical code, currency symbol, currency minimal usable unit and currency name in cyrillic. Second table contains date when rates was taken from central bank, currency id which is foreign key to first table, and currency rate.

--- website/converter_frontend/models.py
+++ website/converter_frontend/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+class Currencies(models.Model):
+    id = models.IntegerField(
+        primary_key=True,
+    )
+    iso = models.CharField(
+        max_length=3,
+        unique=True,
+        null=False,
+    )
+    num_code = models.SmallIntegerField
+    symbol = models.CharField(
+        max_length=5,
+        blank=True,
+        null=True,
+    )
+    unit = models.IntegerField
+    currency_name = models.CharField(max_length=70)
+
+
+class Rates(models.Model):
+    date = models.DateField()
+    curr_id = models.ForeignKey(
+        Currencies, on_delete=models.CASCADE
+    )
+    rate = models.DecimalField(
+        max_digits=10,
+        decimal_places=4,
+        default=1,
+    )
