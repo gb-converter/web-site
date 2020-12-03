0651b0c8694d5a4e167b0b9af7286a4f4a083d8b
Valerii Tsoy <17a0120@gmail.com>
2020-12-03 14:33:54 +0300

    Добавил логгер, парсинг data.json, логика конвертора, подправил шаблон converter.html (обязательность заполнения кол-ва валюты и вывод результата калькуляции и знака валюты)

--- website/converter_frontend/views.py
+++ website/converter_frontend/views.py
@@ -3,6 +3,9 @@ import json
 
 from django.shortcuts import render
 from datetime import datetime
+from loguru import logger
+
+logger.add('logg.txt', encoding='UTF-8', format="\n{time} {level} {message}", level="INFO")
 
 
 def contacts(request):
@@ -21,6 +24,13 @@ def contacts(request):
 
 
 def converter(request):
+
+    from_currency = request.POST['from_currency']
+
+    amount_of_currency_from = float(request.POST['amount_of_currency_from'])
+
+    to_currency = request.POST['to_currency']
+
     path = os.path.join((os.getcwd()), 'data_file.json')
 
     # first check if we have datafile, and download if it is not present
@@ -37,6 +47,7 @@ def converter(request):
         with open(path, 'r',  encoding='utf-8') as data:
             currencies_data = json.load(data)
     except (json.JSONDecodeError, FileNotFoundError):
+        logger.error(f'Ошибка Json файла')
         currencies_data = {
             "Date": datetime.now().date().strftime("%d.%m.%Y"),
             "Недоступно": {
@@ -47,6 +58,31 @@ def converter(request):
                 "Курс": "1,0000"
             }
         }
+
+    # Парсим json
+    for key, val in currencies_data.items():
+        if type(val) == str:
+            continue
+        elif key == from_currency:
+            f_currency = float(val['Курс'].replace(',', '.'))
+            if val.get('Символ') is None:
+                f_symbol = 'None'
+            else:
+                f_symbol = val['Символ']
+        elif key == to_currency:
+            t_currency = float(val['Курс'].replace(',', '.'))
+            if val.get('Символ') is None:
+                t_symbol = 'None'
+            else:
+                t_symbol = val['Символ']
+
+    if from_currency == to_currency:
+        culc_result = amount_of_currency_from
+        symbol = f_symbol
+    else:
+        culc_result = round((f_currency * amount_of_currency_from) / t_currency, 4)
+        symbol = t_symbol
+
 
     # Extract date from dictionary
     currencies_date = currencies_data.pop("Date")
@@ -65,6 +101,8 @@ def converter(request):
         'converter_frontend/converter.html',
         {
             'currency_info': currencies_data,
-            'currencies_rate_date': currencies_date
+            'currencies_rate_date': currencies_date,
+            'converter': culc_result,
+            'symbol': symbol
         }
     )
