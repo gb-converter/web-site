e883a4e3e46a278ab8e7a7879e70957c7a0d1fe2
Viktor Filinskij <v.filinskij@gmail.com>
2020-12-08 18:23:34 +0200

    Add comments with explanations

--- website/api/serializers.py
+++ website/api/serializers.py
@@ -5,6 +5,10 @@ from converter_frontend.models import Currencies, Rates
 
 # Serialisation - translating a data structure or object state
 # into a format that can be stored
+
+# Serializers allow complex data such as querysets and model instances to be converted to native Python datatypes
+# that can then be easily rendered into JSON, XML or other content types. Serializers also provide deserialization,
+# allowing parsed data to be converted back into complex types, after first validating the incoming data.
 class CurrenciesSerializer(serializers.ModelSerializer):
 
     class Meta:
